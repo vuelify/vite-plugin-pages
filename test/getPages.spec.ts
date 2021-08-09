778ef6bd8ccb4950278eab829866c444e41f5218
Ben <vuelify@protonmail.com>
2021-08-09 12:06:40 +1000

    update test for ignore/extend features

--- test/getPages.spec.ts
+++ test/getPages.spec.ts
@@ -1,18 +1,11 @@
-import { should } from "chai";
-import { getPages } from "../src/getPages";
+import { should } from 'chai';
+import { getPages } from '../src/getPages';
 
 should();
 
 function checkRouterObject(
   routerObject: any,
-  {
-    nameValue,
-    componentValue,
-    renderValue,
-    pathValue,
-    baseValue,
-    isChildValue,
-  }: any
+  { nameValue, componentValue, renderValue, pathValue, baseValue, isChildValue }: any,
 ) {
   const { name, component, meta, path, base, isChild } = routerObject;
   const { _render } = meta;
@@ -25,31 +18,31 @@ function checkRouterObject(
   isChild.should.equal(isChildValue);
 }
 
-describe("getPages(pathToPages: string)", function () {
-  it("correctly parses index file path into a router object", async function () {
-    const pages = await getPages("./test/pages", "./test/layouts");
+describe('getPages(pathToPages: string)', function () {
+  it('correctly parses index file path into a router object', async function () {
+    const pages = await getPages('./test/pages', './test/layouts', [], undefined);
     const [index, spa] = pages;
 
     checkRouterObject(index, {
-      nameValue: "Index",
-      componentValue: "./test/pages/$index.vue",
-      renderValue: "pre",
-      pathValue: "/",
-      baseValue: "root",
+      nameValue: 'Index',
+      componentValue: './test/pages/$index.vue',
+      renderValue: 'pre',
+      pathValue: '/',
+      baseValue: 'root',
       isChildValue: false,
     });
   });
 
-  it("correctly parses spa file path into a router object", async function () {
-    const pages = await getPages("./test/pages", "./test/layouts");
+  it('correctly parses spa file path into a router object', async function () {
+    const pages = await getPages('./test/pages', './test/layouts', [], undefined);
     const [index, spa] = pages;
 
     checkRouterObject(spa, {
-      nameValue: "Spa",
-      componentValue: "./test/pages/spa.vue",
-      renderValue: "spa",
-      pathValue: "/spa",
-      baseValue: "spa",
+      nameValue: 'Spa',
+      componentValue: './test/pages/spa.vue',
+      renderValue: 'spa',
+      pathValue: '/spa',
+      baseValue: 'spa',
       isChildValue: false,
     });
   });
